6553f1edbff61dd7ca3c8d032f4985ce90f2fc71
Eric Shaw <ericshaw.linux@gmail.com>
2017-06-18 11:48:16 -0400

    Added the other shapes

--- src/shape.rs
+++ src/shape.rs
@@ -10,4 +10,46 @@ pub static L_SHAPE: [Point; 4] = [
     Point { x: 2, y: 5 },
 ];
 
+pub static J_SHAPE: [Point; 4] = [
+    Point { x: 0, y: 5 },
+    Point { x: 1, y: 5 },
+    Point { x: 2, y: 5 },
+    Point { x: 2, y: 4 },
+];
+
+pub static O_SHAPE: [Point; 4] = [
+    Point { x: 0, y: 4 },
+    Point { x: 0, y: 5 },
+    Point { x: 1, y: 4 },
+    Point { x: 1, y: 5 },
+];
+
+pub static T_SHAPE: [Point; 4] = [
+    Point { x: 0, y: 4 },
+    Point { x: 0, y: 5 },
+    Point { x: 0, y: 6 },
+    Point { x: 1, y: 5 },
+];
+
+pub static S_SHAPE: [Point; 4] = [
+    Point { x: 0, y: 6 },
+    Point { x: 0, y: 5 },
+    Point { x: 1, y: 5 },
+    Point { x: 1, y: 4 },
+];
+
+pub static Z_SHAPE: [Point; 4] = [
+    Point { x: 0, y: 4 },
+    Point { x: 0, y: 5 },
+    Point { x: 1, y: 5 },
+    Point { x: 1, y: 6 },
+];
+
+pub static I_SHAPE: [Point; 4] = [
+    Point { x: 0, y: 4 },
+    Point { x: 1, y: 4 },
+    Point { x: 2, y: 4 },
+    Point { x: 3, y: 4 },
+];
+
 pub static NO_SHAPE: [Point; 0] = [];
