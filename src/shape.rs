1a16349bf68d97064a920ca910a4a771022944be
Eric Shaw <ericshaw.linux@gmail.com>
2017-06-20 19:14:21 -0400

    Added make shape tool and made more shapes needs work
    Should the shapes be centered?

--- src/shape.rs
+++ src/shape.rs
@@ -29,7 +29,13 @@ pub fn next_shape() -> &'static Shape {
     
     match n {
         1 => &L_SHAPES,
-        _ => &L_SHAPES,
+        2 => &J_SHAPES,
+        3 => &S_SHAPES,
+        4 => &Z_SHAPES,
+        5 => &T_SHAPES,
+        6 => &I_SHAPES,
+        7 => &O_SHAPES,
+        _ => panic!("no such shape"),
     }
 }
 
@@ -44,11 +50,45 @@ pub static L_SHAPES: Shape = [
     [Point { x: 0, y: 1 }, Point { x: 1, y: 1 }, Point { x: 2, y: 1 }, Point { x: 2, y: 0 }],
 ];
 
+pub static J_SHAPES: Shape = [
+    [Point { x: 1, y: 0 }, Point { x: 1, y: 1 }, Point { x: 1, y: 2 }, Point { x: 0, y: 2 }],
+    [Point { x: 2, y: 1 }, Point { x: 1, y: 1 }, Point { x: 0, y: 1 }, Point { x: 0, y: 0 }],
+    [Point { x: 0, y: 2 }, Point { x: 0, y: 1 }, Point { x: 0, y: 0 }, Point { x: 1, y: 0 }],
+    [Point { x: 0, y: 0 }, Point { x: 1, y: 0 }, Point { x: 2, y: 0 }, Point { x: 2, y: 1 }],
+];
+
+pub static S_SHAPES: Shape = [
+    [Point { x: 2, y: 0 }, Point { x: 1, y: 0 }, Point { x: 1, y: 1 }, Point { x: 0, y: 1 }],
+    [Point { x: 0, y: 0 }, Point { x: 0, y: 1 }, Point { x: 1, y: 1 }, Point { x: 1, y: 2 }],
+    [Point { x: 2, y: 0 }, Point { x: 1, y: 0 }, Point { x: 1, y: 1 }, Point { x: 0, y: 1 }],
+    [Point { x: 0, y: 0 }, Point { x: 0, y: 1 }, Point { x: 1, y: 1 }, Point { x: 1, y: 2 }],
+];
+
+pub static Z_SHAPES: Shape = [
+    [Point { x: 0, y: 0 }, Point { x: 1, y: 0 }, Point { x: 1, y: 1 }, Point { x: 2, y: 1 }],
+    [Point { x: 1, y: 0 }, Point { x: 1, y: 1 }, Point { x: 0, y: 1 }, Point { x: 0, y: 2 }],
+    [Point { x: 0, y: 0 }, Point { x: 1, y: 0 }, Point { x: 1, y: 1 }, Point { x: 2, y: 1 }],
+    [Point { x: 1, y: 0 }, Point { x: 1, y: 1 }, Point { x: 0, y: 1 }, Point { x: 0, y: 2 }],
+];
+
+pub static T_SHAPES: Shape = [
+    [Point { x: 0, y: 0 }, Point { x: 1, y: 0 }, Point { x: 2, y: 0 }, Point { x: 1, y: 1 }],
+    [Point { x: 1, y: 0 }, Point { x: 1, y: 1 }, Point { x: 1, y: 2 }, Point { x: 0, y: 1 }],
+    [Point { x: 2, y: 1 }, Point { x: 1, y: 1 }, Point { x: 0, y: 1 }, Point { x: 1, y: 0 }],
+    [Point { x: 1, y: 0 }, Point { x: 1, y: 1 }, Point { x: 1, y: 2 }, Point { x: 2, y: 1 }],
+];
+
+pub static I_SHAPES: Shape = [
+    [Point { x: 0, y: 0 }, Point { x: 0, y: 1 }, Point { x: 0, y: 2 }, Point { x: 0, y: 3 }],
+    [Point { x: 0, y: 0 }, Point { x: 1, y: 0 }, Point { x: 2, y: 0 }, Point { x: 3, y: 0 }],
+    [Point { x: 0, y: 0 }, Point { x: 0, y: 1 }, Point { x: 0, y: 2 }, Point { x: 0, y: 3 }],
+    [Point { x: 0, y: 0 }, Point { x: 1, y: 0 }, Point { x: 2, y: 0 }, Point { x: 3, y: 0 }],
+];
+
 pub static O_SHAPES: Shape = [
     [Point { x: 0, y: 0 }, Point { x: 0, y: 1 }, Point { x: 1, y: 1 }, Point { x: 1, y: 0 }],
     [Point { x: 0, y: 0 }, Point { x: 0, y: 1 }, Point { x: 1, y: 1 }, Point { x: 1, y: 0 }],
     [Point { x: 0, y: 0 }, Point { x: 0, y: 1 }, Point { x: 1, y: 1 }, Point { x: 1, y: 0 }],
     [Point { x: 0, y: 0 }, Point { x: 0, y: 1 }, Point { x: 1, y: 1 }, Point { x: 1, y: 0 }],
 ];
-
 
